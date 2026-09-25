44cf1e9f7008353ddf8949afecef2e0c3efa86dc
JamesObidi <110919284+JamesObidi@users.noreply.github.com>
2026-09-25 08:34:58 +0100

    Rename dashboard to home and add login route

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def dashboard():
-    return render_template("dashboard.html")
+def home():
+    return render_template("index.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
